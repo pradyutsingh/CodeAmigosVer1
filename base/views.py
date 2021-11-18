5813fcf5a1e3926db96ccf2b76176b939de05536
unknown <pradyutsingh10@gmail.com>
2021-11-18 18:36:57 +0530

    init

---
+++ base/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import redirect, render
+from .models import Room, Topic
+from .forms import RoomForm
+
+
+def home(request):
+    rooms = Room.objects.all()
+    topics = Topic.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
+
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    # this will fill our form instance with the room values we want to edit
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, "base/delete.html", {'obj': room})
